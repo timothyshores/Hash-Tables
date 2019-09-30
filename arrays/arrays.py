0dc8634c4ee39a870761ccb242e9818b77f3968f
Timothy Shores <timothymshores@gmail.com>
2019-09-30 12:30:59 -0500

    completed arrays.py

--- arrays/arrays.py
+++ arrays/arrays.py
@@ -11,14 +11,38 @@ class DynamicArray:
         self.storage[self.count] = value
         self.count += 1
 
-    def insert():
-        pass
+    def insert(self, value, index):
+        if self.count >= self.capacity:
+            self.resize_array
+
+        for i in range(self.count, index, -1):
+            self.storage[i] = self.storage[i - 1]
+
+        self.storage[index] = value
+        self.count += 1
+
+    def remove(self, index):
+        value = self.storage[index]
+        for i in range(index, self.count - 1, 1):
+            self.storage[i] = self.storage[i + 1]
+        self.count -= 1
+        return value
 
     def print():
-        pass
+        for value in self.storage:
+            print(value)
 
     def resize_array():
-        pass
+        # double the capacity
+        self.capacity *= 2
+        # crate a new array called storage with elements containing None
+        new_storage = [None] * self.capacity
+        # loop through the old array self.count number of times
+        for i in range(self.count):
+            # assign the ith element from self.storage to new_storage
+            new_storage[i] = self.storage[i]
+        # assign self.storage to new_storage
+        self.storage = new_storage
 
-    def add_to_front():
-        pass
+    def add_to_front(self, value):
+        self.insert(value, 0)
