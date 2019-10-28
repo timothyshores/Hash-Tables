b59b0932d2016659617f055a0118a97c06d97b58
Timothy Shores <timothymshores@gmail.com>
2019-10-28 17:19:36 -0500

    Added notes from lecutre

--- arrays/arrays.py
+++ arrays/arrays.py
@@ -1,24 +1,36 @@
 class DynamicArray:
-    def __init__(self, capacity=0):
+    # initialize DynamicArray class with default of capacity 8
+    def __init__(self, capacity=8):
+        # how many elements are in the array
         self.count = 0
+        # how many elements the array can hold
         self.capacity = capacity
+        # reserver space in hardware and operating system memory
         self.storage = [None] * capacity
 
     def append(self, value):
+        # if array is full, resize array
         if self.count >= self.capacity:
             self.resize_array()
-
+        # add value to end of the array
         self.storage[self.count] = value
+        # increment count
         self.count += 1
 
+    # insert a value at a given index
     def insert(self, value, index):
+        # check if there's space in storage
         if self.count >= self.capacity:
             self.resize_array
 
+        # shift everything to the right of index to the right
+        # starting from the rigt and move left to prevent overwriting values
         for i in range(self.count, index, -1):
             self.storage[i] = self.storage[i - 1]
 
+        # insert value into index
         self.storage[index] = value
+        # increment count
         self.count += 1
 
     def remove(self, index):
@@ -28,16 +40,17 @@ class DynamicArray:
         self.count -= 1
         return value
 
-    def print():
+    def print(self):
         for value in self.storage:
             print(value)
 
-    def resize_array():
+    def resize_array(self):
         # double the capacity
         self.capacity *= 2
-        # crate a new array called storage with elements containing None
+        # crate a new array called new_storage with self.capacity number of elements with the value set to None
         new_storage = [None] * self.capacity
-        # loop through the old array self.count number of times
+
+        # copy old items into new_storage by looping through the old array self.count number of times
         for i in range(self.count):
             # assign the ith element from self.storage to new_storage
             new_storage[i] = self.storage[i]
@@ -46,3 +59,16 @@ class DynamicArray:
 
     def add_to_front(self, value):
         self.insert(value, 0)
+
+
+arr = DynamicArray(8)
+print(arr.storage)
+arr.insert(0, "z")
+arr.insert(0, "y")
+arr.insert(0, "x")
+arr.insert(0, "w")
+print(arr.storage)
+arr.append("a")
+arr.append("b")
+arr.append("c")
+arr.append("d")
